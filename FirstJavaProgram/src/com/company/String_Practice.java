5ca3b780e5e1c95775a0fe2e90c80696e8f05c22
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-29 18:33:17 -0500

    git commit -m "third commit"

---
+++ FirstJavaProgram/src/com/company/String_Practice.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class String_Practice {
+    public static void main(String[] args){
+        //Problem 1
+        String name = "Nasim Qureshi";
+        name = name.toLowerCase();
+        System.out.println(name.toLowerCase());
+    }
+}
