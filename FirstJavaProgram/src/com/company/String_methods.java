b0ea969d3a4b40402197b26e374ecec8c5a91b5f
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-29 18:19:18 -0500

    git commit -m "second commit"

--- FirstJavaProgram/src/com/company/String_methods.java
+++ FirstJavaProgram/src/com/company/String_methods.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Locale;
+
 public class String_methods {
     public static void main(String[] args) {
      String a = "Nasim";
+
+     String lstring = a.toLowerCase();
         System.out.println(a);
     }
 }
