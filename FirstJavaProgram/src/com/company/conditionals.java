7f16f49550b0360d0f672ac0753a3d368a4d20a1
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-29 19:39:17 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/conditionals.java
+++ FirstJavaProgram/src/com/company/conditionals.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class conditionals {
     public static void main(String[] args){
-        int age = 28;
-        if(age > 18){
+        int age = 8;
+        if(age != 18){
             System.out.println("Yes you can drive");
         }
         else {
