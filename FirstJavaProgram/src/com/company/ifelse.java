21bd5452235f2bd2f45c3df1daf14e4b4c8a4623
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-29 21:08:46 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/ifelse.java
+++ FirstJavaProgram/src/com/company/ifelse.java
@@ -2,23 +2,26 @@ package com.company;
 import java.util.Scanner;
 
 public class ifelse {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int age;
         System.out.println("Enter your age :");
         Scanner sc = new Scanner(System.in);
         age = sc.nextInt();
 
-        if(age > 56){
-            System.out.println("Experienced");
+
+        switch (age) {
+            case 18:
+                System.out.println("You are going to become an adult!");
+                break;
+            case 23:
+                System.out.println("You are going to get a job");
+                break;
+            case 60:
+                System.out.println("You are going to get retired");
+                break;
+            default:
+                System.out.println("Enjoy your life");
         }
-        else if(age > 46){
-            System.out.println("sami-experienced");
-        }
-        else if(age > 36){
-            System.out.println("sami-sami experienced");
-        }
-        else {
-            System.out.println("not experienced");
+
         }
     }
-}
