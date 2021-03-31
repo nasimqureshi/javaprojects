a4025133e35577dfb8ba9489fa3ec4a09f6724f4
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-30 22:08:20 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/for_loop.java
+++ FirstJavaProgram/src/com/company/for_loop.java
@@ -2,9 +2,16 @@ package com.company;
 
 public class for_loop {
     public static void main(String[] args){
-        for (int i = 1; i <= 20; i++){
-            System.out.println("How are you Nasim");
-        }
+//        for (int i = 1; i <= 20; i++){
+//            System.out.println("How are you Nasim");
+//        }
+//        int n = 3;
+//        for (int i= 0; i < n; i++){
+//            System.out.println(2 * + i);
+//        }
+          for(int i = 5; i!=0; i--){
+              System.out.println(i);
+          }
 
     }
 }
