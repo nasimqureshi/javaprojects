9d14fc2b23d2e7ecc093993f424a44035698c858
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-30 19:53:38 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/loops.java
+++ FirstJavaProgram/src/com/company/loops.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class loops {
     public static void main(String[] args){
-       int i = 1;
-       while (i <= 5) {
+       int i = 100;
+       while (i <= 155) {
            System.out.println(i);
            i++;
        }
+        System.out.println("End of while loop");
+       }
+
     }
-}
+
