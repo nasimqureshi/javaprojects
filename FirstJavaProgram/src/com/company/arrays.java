ba7f0ea8c3cd736e2d715f5e307ee37a91f7f6ec
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-31 22:17:48 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/arrays.java
+++ FirstJavaProgram/src/com/company/arrays.java
@@ -1,22 +1,25 @@
 package com.company;
 
 public class arrays {
-    public static void main(String[] args){
-     //   int [] marks = new int[5];
+    public static void main(String[] args) {
+        //   int [] marks = new int[5];
 //        marks[0] = 100;
 //        marks[1] = 60;
 //        marks[2] = 70;
 //        marks[3] = 90;
 //        marks[4] = 86;
 //
-        int [] marks = {90, 70, 80, 71, 98};
+        int[] marks = {90, 70, 80, 71, 98};
 //        System.out.println(marks[4]);
 //        System.out.println(marks.length);
 
         //Displaying the Array (for loop)
-        for(int i =0; i <  marks.length; i++){
-            System.out.println(marks [i]);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
         }
-
+        System.out.println("Printing array in reverse order");
+        for (int i=marks.length -1; i >= 0; i--) {
+            System.out.println(marks[i]);
     }
+}
 }
