faba25fdbff817f799d652307e91923b3f9380d5
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-31 20:43:11 -0500

    git commit -m "first commit"

---
+++ FirstJavaProgram/src/com/company/break_and_continue.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class break_and_continue {
+    public static void main(String[] args){
+        //Break and continue using loops!
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+            System.out.println("Nasim is great");
+            System.out.println("Java is amazing");
+        }
+    }
+}
