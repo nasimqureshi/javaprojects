47ca74b6f0c776e87fb11ec5c2d94d600d6a8f53
nasimqureshi <qureshi.nasim@yahoo.com>
2021-03-31 21:54:14 -0500

    git commit -m "first commit"

--- FirstJavaProgram/src/com/company/arrays.java
+++ FirstJavaProgram/src/com/company/arrays.java
@@ -2,13 +2,15 @@ package com.company;
 
 public class arrays {
     public static void main(String[] args){
-        int [] marks = new int[5];
-        marks[0] = 100;
-        marks[1] = 60;
-        marks[2] = 70;
-        marks[3] = 90;
-        marks[4] = 86;
-        System.out.println(marks[3]);
+     //   int [] marks = new int[5];
+//        marks[0] = 100;
+//        marks[1] = 60;
+//        marks[2] = 70;
+//        marks[3] = 90;
+//        marks[4] = 86;
+//
+        int [] marks = {10, 70, 80, 71, 98};
+        System.out.println(marks[4]);
 
     }
 }
