eb6c69f2735d429120396b7fb674742066461c50
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-05 10:25:28 -0500

    git commit -m "second commit"

--- FirstJavaProgram/src/com/company/args.java
+++ FirstJavaProgram/src/com/company/args.java
@@ -2,12 +2,20 @@ package com.company;
 
 
         public class args {
+            static int sum(int a, int b) {
+                return a + b;
+            }
+            static int sum(int a, int b, int c) {
+                return a + b + c;
+            }
             static int sum(int a, int b, int c, int d) {
                 return a + b + c + d;
             }
 
             public static void main(String[] args) {
                 System.out.println("Welcome to Args");
+                System.out.println("The sum of 9 and 6 is: " + sum(9, 6));
+                System.out.println("The sum of 9 and 6 is: " + sum(9, 6, 10));
                 System.out.println("The sum of 9 and 6 is: " + sum(9, 6, 10, 12));
             }
 
