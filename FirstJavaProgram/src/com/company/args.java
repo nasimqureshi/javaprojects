5eb137915810e3ad27eccdaaaf2521035c06b604
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-05 09:47:30 -0500

    git commit -m "first commit"

---
+++ FirstJavaProgram/src/com/company/args.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class args {
+    static int sum(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args){
+        System.out.println("Welcome to Args");
+        System.out.println("The sum of 4 and 5 is:" + sum(4, 5));
+    }
+}
