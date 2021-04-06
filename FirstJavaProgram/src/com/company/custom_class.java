d9fab6d45cd166a7213e14f33707a6bd15667592
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-06 10:35:28 -0500

    git commit -m "second commit"

--- FirstJavaProgram/src/com/company/custom_class.java
+++ FirstJavaProgram/src/com/company/custom_class.java
@@ -3,18 +3,27 @@ package com.company;
 class Employee {
     int id;
     String name;
+    public void pintDetails(){
+        System.out.println("My id is: "  + id);
+        System.out.println("ans my name is:" + name);
+    }
 }
 public class custom_class {
     public static void main(String[] args) {
         System.out.println("This is a custom class");
         //Instantiating new object.
         Employee Nasim  = new Employee();
+        Employee Qamar = new Employee();
 
         //setting attributes.
         Nasim.id =13;
         Nasim.name = "CodeWithNasim";
-        System.out.println(Nasim.id);
-        System.out.println(Nasim.name);
+        Qamar.id = 18;
+        Qamar.name = "qamar qureshi";
+        //System.out.println(Nasim.id);
+        //System.out.println(Nasim.name);
+        Nasim.pintDetails();
+        Qamar.pintDetails();
 
     }
 
