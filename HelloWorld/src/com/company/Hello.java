73948410d0aa4fd2c0510f1d96f39e2350597d0d
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-07 09:31:08 -0500

    git commit -m "my commit"

--- HelloWorld/src/com/company/Hello.java
+++ HelloWorld/src/com/company/Hello.java
@@ -3,5 +3,8 @@ package com.company;
 public class Hello {
     public static void main(String[] arg){
         System.out.println("Hello World");
+
+        int myFirstNumber = 5 + 20 + 30;
+        System.out.println(myFirstNumber);
     }
 }
