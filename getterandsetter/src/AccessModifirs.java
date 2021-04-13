04898084f91fdb53148b4e83b8ddd78da3141e5e
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-12 22:46:03 -0500

    get commit -m "first commit"

--- getterandsetter/src/AccessModifirs.java
+++ getterandsetter/src/AccessModifirs.java
@@ -17,14 +17,22 @@ class Cylinder{
     public void setHeight(int height) {
         this.height = height;
     }
+    public double volume(){
+        return 3.142*radius*radius*height;
+    }
 }
 
 public class AccessModifirs {
     public static void main(String[] args) {
+
         Cylinder myCylinder = new Cylinder();
         myCylinder.setHeight(13);
         System.out.println(myCylinder.getHeight());
         myCylinder.setRadius(9);
         System.out.println(myCylinder.getRadius());
+
+        // System.out.println(myCylinder.surfaceArea());
+
+        System.out.println(myCylinder.volume());
     }
 }
