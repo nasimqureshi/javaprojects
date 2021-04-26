7892e57ce41e1650661e69ac2ea618dbb0943289
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-25 21:55:30 -0500

    "first commit"

---
+++ MyJava/src/com/company/this_supper.java
@@ -0,0 +1,9 @@
+package com.company;
+class MyClass{
+    int a;
+}
+public class this_supper {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ switch_statement/src/switch_statement.java
@@ -0,0 +1,2 @@
+public class switch_statement {
+}
