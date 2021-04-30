a0dff52e35b5dcfb318c41b886b4e6ee7b7092be
nasimqureshi <qureshi.nasim@yahoo.com>
2021-04-30 13:03:11 -0500

    "second commit"

---
+++ JavaLearning/src/com/company/mehtods.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class mehtods {
+    public static void main(String[] args) {
+      hello();
+      hello();
+      hello();
+      hello();
+      hello();
+      hello();
+    }
+    static void hello( ){
+        System.out.println("Hello Nasim");
+    }
+
+
+}
