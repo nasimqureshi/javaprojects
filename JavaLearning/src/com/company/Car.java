ade3bacb08cbca5b39c20294128bab2af66a7985
nasimqureshi <qureshi.nasim@yahoo.com>
2021-05-04 11:14:07 -0500

    "second commit"

---
+++ JavaLearning/src/com/company/Car.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Car {
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 55000.50;
+
+    void  drive() {
+        System.out.println("You can drive this car");
+    }
+    void brake(){
+        System.out.println("You can stop the card by applying brake");
+
+    }
+}
---
+++ JavaLearning/src/com/company/OOP.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class OOP {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println(myCar.color);
+        System.out.println(myCar.price);
+        System.out.println(myCar.model);
+        System.out.println(myCar.year);
+
+    }
+}
