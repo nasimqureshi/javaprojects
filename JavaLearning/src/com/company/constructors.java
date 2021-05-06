0a6ea2fa6f7575d63bbfe1360e6d9c53201335cb
nasimqureshi <qureshi.nasim@yahoo.com>
2021-05-06 14:16:56 -0500

    "second commit"

--- JavaLearning/src/com/company/constructors.java
+++ JavaLearning/src/com/company/constructors.java
@@ -2,10 +2,16 @@ package com.company;
 
 public class constructors {
     public static void main(String[] args) {
-      Human human = new Human("Nasim Qureshi", 68, 170);
-        System.out.println(human.name);
-        System.out.println(human.age);
-        System.out.println(human.weight);
+      Human human1 = new Human("Nasim Qureshi", 68, 170);
+      Human human2 = new Human("Qamar Qureshi", 65, 150);
+
+        System.out.println(human1.name);
+        System.out.println(human1.age);
+        System.out.println(human1.weight);
+
+        System.out.println(human2.name);
+        System.out.println(human2.age);
+        System.out.println(human2.weight);
     }
 
 }
